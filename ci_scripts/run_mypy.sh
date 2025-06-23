2afb74e371690f20795bc43f3173b879a93dea67
Martin Burchell <martinburchell@gmail.com>
2025-06-23 13:45:46 +0100

    MyPy workflow

---
+++ ci_scripts/run_mypy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -euo pipefail
+
+if [ "$#" != "1" ]; then
+   echo "Usage: $0 <project_root>"
+   exit 1
+fi
+
+PROJECT_ROOT=$1
+
+PYTHON=${HOME}/venv/bin/python
+${PYTHON} -m pip install mypy
+MYPY=${HOME}/venv/bin/mypy
+
+cd "${PROJECT_ROOT}"
+
+${MYPY} --strict
